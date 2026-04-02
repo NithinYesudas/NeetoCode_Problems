185b8c477dd49165cf1a5ea2e05da5cd1df3a935
Nithin Yesudas <85782209+NithinYesudas@users.noreply.github.com>
2026-04-02 23:47:08 +0530

    Add: minimum-height-trees - submission-0

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-0.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        minHeight = float('inf')
+        res = []
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        visited = set()
+        def dfs(i):
+            visited.add(i)
+            maxDepth = 0
+            for neighbor in graph[i]:
+                if neighbor not in visited:
+                    maxDepth = max(1+dfs(neighbor),maxDepth)
+            return maxDepth
+        for i in range(n):
+            visited = set()
+            height = dfs(i)
+            if height<minHeight:
+                res.clear()
+                res.append(i)
+                minHeight=height
+            elif height==minHeight:
+                print(minHeight)
+                res.append(i)
+        return res
+        
