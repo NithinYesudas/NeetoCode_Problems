509b97eee5489ea106cbf6b108fa5aec2e9cc542
Nithin Yesudas <85782209+NithinYesudas@users.noreply.github.com>
2026-04-07 22:32:19 +0530

    Add: find-pivot-index - submission-2

---
+++ Data Structures & Algorithms/find-pivot-index/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for i in range(1,len(nums)):
+            nums[i] = nums[i]+nums[i-1]
+        for i in range(len(nums)):
+            currDiff = nums[-1]-nums[i]
+            if  currDiff== nums[i-1] or (currDiff==0 and i == 0):
+                return i
+        
+        return -1
+
+
+        
