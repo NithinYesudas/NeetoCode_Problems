435016a261e4c1ea52dad96b6d2345fde4304bd8
Nithin Yesudas <85782209+NithinYesudas@users.noreply.github.com>
2026-04-09 01:13:56 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def bactrack(opened,closed,curr):
+            if opened ==0 and closed==0:
+                res.append(curr)
+            if opened>0:
+               bactrack(opened-1,closed,curr+'(')
+            if closed>0 and opened<closed:
+              bactrack(opened,closed-1,curr+')')
+        bactrack(n,n,"")
+        return res
+            
+            
+        
